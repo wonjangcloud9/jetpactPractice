8ad8750a879c9ecb7488afab15f74f87f22b3c94
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-02-06 15:35:17 +0900

    state

--- app/src/main/java/com/example/jetpack_practice2/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_practice2/MainActivity.kt
@@ -7,13 +7,22 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.FavoriteBorder
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -24,14 +33,30 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ImageCard()
+            var isFavorite by rememberSaveable {
+                mutableStateOf(false)
+            }
+            ImageCard(
+                isFavorite = isFavorite,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .fillMaxSize(0.5f),
+            ){
+                favorite ->
+                isFavorite = favorite
+            }
         }
     }
 }
 
 @Composable
-fun ImageCard() {
+fun ImageCard(
+    modifier: Modifier = Modifier,
+    isFavorite: Boolean,
+    onTabFavorite: (Boolean) -> Unit,
+) {
     Card(
+        modifier = modifier,
         shape = RoundedCornerShape(8.dp),
         elevation = 5.dp,
     ) {
@@ -43,6 +68,21 @@ fun ImageCard() {
                 contentDescription = "",
                 contentScale = ContentScale.Crop,
             )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.TopEnd
+            ) {
+                IconButton(onClick = {
+                    onTabFavorite(!isFavorite)
+                }) {
+                    Icon(
+                        imageVector = if (isFavorite) Icons.Default.Favorite else Icons.Default.FavoriteBorder,
+                        contentDescription = "favorite",
+                        tint = Color.White,
+                    )
+                }
+            }
         }
     }
 }
