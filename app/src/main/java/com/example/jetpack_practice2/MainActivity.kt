c39a9cfc22b6447c592aa17e6a6300af6cd5abbc
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-02-06 16:13:09 +0900

    Scaffold & tTextField & Button & SnackBar & CoroutineScope

--- app/src/main/java/com/example/jetpack_practice2/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_practice2/MainActivity.kt
@@ -4,50 +4,68 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.FavoriteBorder
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetpack_practice2.ui.theme.Jetpackpractice2Theme
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalComposeUiApi::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var isFavorite by rememberSaveable {
-                mutableStateOf(false)
+
+            val (text, setValue) = remember {
+                mutableStateOf("")
             }
-            ImageCard(
-                isFavorite = isFavorite,
-                modifier = Modifier
-                    .padding(16.dp)
-                    .fillMaxSize(0.5f),
-            ){
-                favorite ->
-                isFavorite = favorite
+
+            val scaffoldState = rememberScaffoldState()
+
+            val scope = rememberCoroutineScope()
+
+            val keyboardController = LocalSoftwareKeyboardController.current
+
+            Scaffold(
+                scaffoldState = scaffoldState,
+            ) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextField(
+                        value = text,
+                        onValueChange = setValue,
+                    )
+                    Button(onClick = {
+                        keyboardController?.hide()
+                        scope.launch { scaffoldState.snackbarHostState.showSnackbar("Hello $text") }
+                    }) {
+                        Text(text = "클릭")
+                    }
+                }
             }
         }
     }
 }
+
 
 @Composable
 fun ImageCard(
