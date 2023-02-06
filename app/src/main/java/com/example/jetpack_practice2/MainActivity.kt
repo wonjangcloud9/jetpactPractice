6b7841ae32cbd6e6dffac8e846c015251fcdf30f
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2023-02-06 14:53:56 +0900

    image

--- app/src/main/java/com/example/jetpack_practice2/MainActivity.kt
+++ app/src/main/java/com/example/jetpack_practice2/MainActivity.kt
@@ -3,41 +3,46 @@ package com.example.jetpack_practice2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetpack_practice2.ui.theme.Jetpackpractice2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Jetpackpractice2Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            ImageCard()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Jetpackpractice2Theme {
-        Greeting("Android")
+fun ImageCard() {
+    Card(
+        shape = RoundedCornerShape(8.dp),
+        elevation = 5.dp,
+    ) {
+        Box(
+            modifier = Modifier.height(200.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+            )
+        }
     }
 }
